e00f1a752fd84453b7eb5c7ed4b06ba1e057f5e0
Madiyar Aydarkulov <65074114+aidmadiyar89@users.noreply.github.com>
2020-09-25 13:29:29 +0300

    upload files

---
+++ isPowerOfThree.js
@@ -0,0 +1,17 @@
+const isPowerOfThree = (num) => {
+    let current = 1;
+    while (current < num) {
+        current *= 3;
+    }
+
+    return current === num;
+};
+
+/*
+Реализуйте и экспортируйте по умолчанию функцию, которая определяет, является ли переданное число натуральной степенью тройки. Например, число 27 — это третья степень: 3^3, а 81 — это четвёртая: 3^4.
+
+Примеры
+isPowerOfThree(1); // true (3^0)
+isPowerOfThree(2); // false
+isPowerOfThree(9); // true (3^2)
+*/
